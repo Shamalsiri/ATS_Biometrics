c82a3f582d29b43d25ef9fb3c25e1a9aff9c86ac
ssiriwardana <ssiriwardana@accu-time.com>
2023-02-06 10:38:57 -0500

    [SS] Employee Info Activity. UI completed.

---
+++ app/src/main/java/com/siriwardana/ats_biometrics/EmployeeInfo.java
@@ -0,0 +1,14 @@
+package com.siriwardana.ats_biometrics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmployeeInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_info);
+    }
+}
